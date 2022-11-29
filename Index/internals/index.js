ae9ec7b890d423e8907d00b098f36f750b828988
mcooper7649 <55610025+mcooper7649@users.noreply.github.com>
2022-11-29 10:46:26 -0500

    how indexjs can be used to import/export

---
+++ Index/internals/index.js
@@ -0,0 +1,9 @@
+const request = require('./request');
+const response = require('./response');
+
+module.exports = {
+  send: request.send,
+  REQUEST_TIMEOUT: request.REQUEST_TIMEOUT,
+  read: response.read,
+  response: require('./response'),
+};
---
+++ Index/internals/response.js
@@ -0,0 +1,12 @@
+function decrypt(data) {
+  return 'decrypt data';
+}
+
+function read(params) {
+  return decrypt('data');
+}
+
+module.exports = {
+  decrypt,
+  read,
+};
